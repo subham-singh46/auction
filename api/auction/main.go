6021278a05f48fd2dc6fb1706ee45d59204992b8
subham singh <reachsubham46@gmail.com>
2024-09-28 23:54:13 +0530

    added api folder

---
+++ api/auction/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/subham-singh46/auction/server"
+	postgresDb "github.com/subham-singh46/auction/store/pg"
+)
+
+const httpAddress = ":5000"
+
+func main() {
+	log.Printf("Initialising members server")
+
+	log.Printf("Connecting to database...")
+
+	store, err := postgresDb.NewAuctionDbConnector().Connect()
+	if err != nil {
+		log.Panicf("Unable to connect to db, error: %s\n", err)
+	}
+	log.Printf("Db connection established")
+
+	s := server.InitServer(store)
+	if err = s.Start(httpAddress); err != nil {
+		log.Panicf("Failed to initialise server at %s, error: %s\n", httpAddress, err)
+	}
+}
